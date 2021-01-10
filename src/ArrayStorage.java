0c76bb068c00a70f2e68c426f8f83db61fdd58e8
Your Name <safon0306@gmail.com>
2021-01-10 17:14:02 +0300

    Create method save Create method clear Create method get Create method delete Create method getAll Create method size

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,66 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int x = 0; x < storage.length; x++){
+            storage[x] = null;
+        }
     }
 
     void save(Resume r) {
+        for (int x = 0; x <storage.length;x++){
+            if (storage[x] == null) {
+                storage[x] = r;
+                break;
+            }
+            else continue;
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume value = new Resume();
+        try {
+            for (Resume resume : storage) {
+                if (resume.uuid.equals(uuid)) {
+                    value = resume;
+                    break;
+                }
+            }
+        } catch (NullPointerException e){
+            System.out.println("нет такого uuid");
+        }
+        return value;
     }
 
     void delete(String uuid) {
+        try {
+            for (Resume element : storage) {
+                for (int x = 0; x < storage.length; x++) {
+                    if (storage[x].uuid != null) {
+                        if (storage[x].uuid.equals(uuid)) {
+                            storage[x] = null;
+                            for (int y = x + 1; y < storage.length - 1; y++) {
+                                if (storage[y] != null) {
+                                    storage[x] = storage[y];
+                                    storage[y] = null;
+                                } else break;
+                            }
+                        }
+                    } else break;
+                }
+            }
+        } catch (NullPointerException e){
+            System.out.println("нет такого uuid");
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return storage;
     }
 
     int size() {
-        return 0;
+        return storage.length;
     }
 }
